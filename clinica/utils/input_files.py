44473aad84a4533a42d88f8dede9ec7002c19b42
msolal <maelys.solal@cs.ox.ac.uk>
2025-11-20 11:09:58 +0100

    change pet into rec-* for new CAPS / BIDS convention

--- clinica/utils/input_files.py
+++ clinica/utils/input_files.py
@@ -468,7 +468,7 @@ def pet_linear_nii(acq_label, suvr_reference_region, uncropped_image):
     information = {
         "pattern": os.path.join(
             "pet_linear",
-            f"*_trc-{acq_label}_pet_space-MNI152NLin2009cSym{description}_res-1x1x1_suvr-{suvr_reference_region}_pet.nii.gz",
+            f"*_trc-{acq_label}_rec-*_space-MNI152NLin2009cSym{description}_res-1x1x1_suvr-{suvr_reference_region}_pet.nii.gz",
         ),
         "description": "",
         "needed_pipeline": "pet-linear",
